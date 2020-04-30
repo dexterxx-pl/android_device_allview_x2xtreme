4d49fc787fe0ce4da8ec5cbe9a69865df8bcd49e
Kamil Komorek <admin@dexterxx.pl>
2020-04-30 22:05:26 +0200

    step

--- lineage_x2xtreme.mk
+++ lineage_x2xtreme.mk
@@ -1,8 +1,15 @@
-# Inherit device configuration
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Vendor blobs
+$(call inherit-product-if-exists, vendor/allview/x2xtreme/x2xtreme-vendor.mk)
+
+# Device
+$(call inherit-product, device/allview/x2xtreme/device.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 2560
