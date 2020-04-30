7095771bcc2920f04cab43f0ece7c78817b2267d
Kamil Komorek <admin@dexterxx.pl>
2020-04-30 21:45:35 +0200

    current lineage 17.1 build fixes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage.mk
-	
+	$(LOCAL_DIR)/lineage_x2xtreme.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_x2xtreme-user \
+	lineage_x2xtreme-userdebug \
+	lineage_x2xtreme-eng
+
