f14afc764d0aa02d88edba2570c7b6ae5adc3ec0
Kamil Komorek <admin@dexterxx.pl>
2020-05-01 20:00:14 +0200

    TODO commented mtk declarations
    Change-Id: I69a4aaa2d7d7c4278b39d562c84cbca8b3d85082

--- libmtk_symbols/mtk_gui.cpp
+++ libmtk_symbols/mtk_gui.cpp
@@ -2,8 +2,9 @@
 
 extern "C" {
     void _ZN7android14SurfaceControl8setLayerEj(uint32_t);
-
+/* TODO(dexterxx)
     void _ZN7android14SurfaceControl8setLayerEi(int32_t layer){
         _ZN7android14SurfaceControl8setLayerEj(static_cast<uint32_t>(layer));
     }
+*/
 }
