eacfe70106a8aca7451258307a898022b65c8c21
Kamil Komorek <admin@dexterxx.pl>
2020-05-01 13:57:34 +0200

    mtk_ui missing string include
    Change-Id: Iac07eee01d48ca7bdf4b88d420fb5c4b203af2ba

--- libmtk_symbols/mtk_ui.cpp
+++ libmtk_symbols/mtk_ui.cpp
@@ -1,6 +1,7 @@
 #include <ui/GraphicBufferMapper.h>
 #include <ui/PixelFormat.h>
 #include <ui/Rect.h>
+#include <string>
 
 extern "C" {
     void _ZN7android19GraphicBufferMapper9lockYCbCrEPK13native_handlejRKNS_4RectEP13android_ycbcr(buffer_handle_t, uint32_t, const android::Rect&, android_ycbcr*);
